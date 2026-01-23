20d538be3a657a4bbaa5c091244dd6fc8b0b2526
Hưng Trương <hungtqse182075@fpt.edu.vn>
2026-01-23 12:50:20 +0700

    feat: get tour by status

--- src/main/java/org/foodie_tour/modules/tours/controller/TourController.java
+++ src/main/java/org/foodie_tour/modules/tours/controller/TourController.java
@@ -4,13 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.foodie_tour.modules.tours.dto.request.TourRequest;
 import org.foodie_tour.modules.tours.dto.response.TourResponse;
+import org.foodie_tour.modules.tours.enums.TourStatus;
 import org.foodie_tour.modules.tours.service.TourService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tour")
@@ -22,5 +22,12 @@ public class TourController {
     public ResponseEntity<TourResponse> createTour(@Valid @RequestBody TourRequest tourRequest) {
         TourResponse response = tourService.createTour(tourRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<TourResponse>> getAllTours(
+            @RequestParam(required = false) TourStatus status) {
+        List<TourResponse> tours = tourService.getAllTours(status);
+        return ResponseEntity.status(HttpStatus.OK).body(tours);
     }
 }
--- src/main/java/org/foodie_tour/modules/tours/repository/TourRepository.java
+++ src/main/java/org/foodie_tour/modules/tours/repository/TourRepository.java
@@ -1,10 +1,13 @@
 package org.foodie_tour.modules.tours.repository;
 
 import org.foodie_tour.modules.tours.entity.Tour;
+import org.foodie_tour.modules.tours.enums.TourStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TourRepository extends JpaRepository<Tour, Long> {
     boolean existsByTourName(String tourName);
+    List<Tour> findByTourStatus(TourStatus tourStatus);
 }
