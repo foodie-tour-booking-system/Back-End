e1f80659cac995b111ddec78c4b8639fe591bedc
Hưng Trương <hungtqse182075@fpt.edu.vn>
2026-01-24 22:33:56 +0700

    feat: get all dishes by status

--- src/main/java/org/foodie_tour/modules/tours/controller/DishController.java
+++ src/main/java/org/foodie_tour/modules/tours/controller/DishController.java
@@ -4,10 +4,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.foodie_tour.modules.tours.dto.request.DishRequest;
 import org.foodie_tour.modules.tours.dto.response.DishResponse;
+import org.foodie_tour.modules.tours.enums.DishStatus;
 import org.foodie_tour.modules.tours.service.DishService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/dishes")
@@ -20,5 +23,14 @@ public class DishController {
     public ResponseEntity<DishResponse> createDish(@Valid @RequestBody DishRequest dishRequest) {
         DishResponse response = dishService.createDish(dishRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<DishResponse>> getAllDishes(
+            @RequestParam(required = false)
+            DishStatus dishStatus
+    ) {
+        List<DishResponse> response = dishService.getAllDishes(dishStatus);
+        return ResponseEntity.ok(response);
     }
 }
