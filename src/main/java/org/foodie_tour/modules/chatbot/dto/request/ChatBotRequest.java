3cbfedd23655a13750729b136a09aa58b63e34e1
vdhxi <83151403+vdhxi@users.noreply.github.com>
2026-03-16 23:23:30 +0700

    feat: implement chat memory for rag chatbot

--- src/main/java/org/foodie_tour/modules/chatbot/dto/request/ChatBotRequest.java
+++ src/main/java/org/foodie_tour/modules/chatbot/dto/request/ChatBotRequest.java
@@ -11,5 +11,6 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ChatBotRequest {
+    String conversationId;
     String prompt;
 }
