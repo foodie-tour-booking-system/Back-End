c6c15cc1dbedacd92b1100a51b1a84dba087466b
DaoNQ1408 <nguyenquangdao1408@gmail.com>
2026-03-17 21:58:22 +0700

    fix(TourImage): make api public

--- src/main/resources/rbac/insert-role.sql
+++ src/main/resources/rbac/insert-role.sql
@@ -19,3 +19,14 @@ WHERE r.name = 'ADMIN'
   AND NOT EXISTS (
     SELECT 1 FROM public.employee e WHERE e.email = 'felixiter.travel@gmail.com'
   );
+
+
+INSERT INTO public.employee (role_id, employee_name, email, password, status, created_at, updated_at)
+SELECT r.role_id, 'Employee', 'employee@gmail.com',
+       '$2a$10$IXBBliXi8Xs73uioBUnJIeeDECtPhi2nM6yUE8JWPYqSsEEu3cKbS',
+       'ACTIVE', NOW(), NOW()
+FROM public.role r
+WHERE r.name = 'EMPLOYEE'
+  AND NOT EXISTS (
+    SELECT 1 FROM public.employee e WHERE e.email = 'employee@gmail.com'
+);
